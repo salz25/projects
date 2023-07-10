c02ad62101768f9a24050b68d0f068e0f939b77a
salz25 <tzzsal15@gmail.com>
2023-07-10 10:05:04 +0700

    fix

--- karyawan.php
+++ karyawan.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Data Karyawan</title>
+    <title>Data Karyawan Mba</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
